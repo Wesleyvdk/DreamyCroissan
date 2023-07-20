f247b200a3651c0fb86918e92b7bd43745e8d52c
wesleyvdk <shinxisrl@outlook.com>
2023-07-20 13:10:54 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,60 @@
+import type { Metadata } from "next";
+import Link from "next/link";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@/components/analytics";
+import { ModeToggle } from "@/components/mode-toggle";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "DreamyCroissant",
+  description: "Take stories to the next level",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="min-w-fit max-w-full mx-auto py-10 px-4">
+            <header>
+              <div className="flex items-center justify-between">
+                <ModeToggle />
+                <nav className="ml-auto text-base font-medium space-x-2">
+                  <Link
+                    className="hover:bg-gradient-conic from-sky-600 via-purple-950 to-sky-600"
+                    href="/"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    className="hover:bg-gradient-conic from-sky-600 via-blue-950 to-sky-600"
+                    href="/about"
+                  >
+                    About
+                  </Link>
+                  <Link
+                    className="hover:bg-gradient-conic from-sky-600 via-blue-950 to-sky-600"
+                    href="/teams"
+                  >
+                    Teams
+                  </Link>
+                </nav>
+              </div>
+            </header>
+            <main>{children}</main>
+          </div>
+          <Analytics />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ pages/stories.tsx
@@ -0,0 +1,111 @@
+import Image from "next/image";
+import "../app/globals.css";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+
+      <div className="relative flex place-items-center after:absolute after:-z-20 after:h-[280px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <div className="w-72">
+          <p className="mb-5">
+            Welcome to TaleVerse, your gateway to a world of captivating
+            stories! We believe that every story has the power to transport
+            readers to new realms of imagination and emotion. Our platform is
+            dedicated to providing a diverse collection of tales from talented
+            storytellers across the globe.
+          </p>
+          <p className="mb-5">
+            At TaleVerse, you can dive into a myriad of genres, from fantasy and
+            science fiction to romance and mystery. With our user-friendly
+            interface, you can easily discover stories based on your
+            preferences, sorted by upload date, name, or rating. If you're an
+            aspiring author, you can share your own tales and connect with
+            readers who share your passion for storytelling.
+          </p>
+          <p className="mb-5">
+            Our commitment to creating an engaging community extends to the next
+            level with secure login options through Discord. With NextAuth
+            integration, you can rest assured that your reading journey is both
+            seamless and protected.
+          </p>
+          <p className="mb-5">
+            Join us on this literary adventure and immerse yourself in the magic
+            of words. Uncover new worlds, follow unforgettable characters, and
+            be a part of the ever-growing TaleVerse community. Happy reading!
+          </p>
+        </div>
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore the Next.js 13 playground.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  );
+}
