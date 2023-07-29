7d621a23dfcbc12ee8d76ea7043aa274016d5523
wesleyvdk <shinxisrl@outlook.com>
2023-07-29 18:18:03 +0200

    updates

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,11 +1,13 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
+const scopes = ["identify"].join(" ");
 export const authOptions: NextAuthOptions = {
   providers: [
     DiscordProvider({
       clientId: `${process.env.DISCORD_CLIENT_ID}`,
       clientSecret: `${process.env.DISCORD_CLIENT_SECRET}`,
+      authorization: { params: { scope: scopes } },
     }),
   ],
 };
